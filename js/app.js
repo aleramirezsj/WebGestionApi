36bfe7d6dfc94d579587fc9f90ea6126cc2f6f34
aleramirezsj <aleramirezsj@gmail.com>
2023-06-09 14:28:44 -0300

    eliminar funcionando

--- js/app.js
+++ js/app.js
@@ -1,23 +1,30 @@
 const movies = {
-    render: () => {
+    //definimos la función que obtiene todas las peliculas
+    obtenerTodos: () => {
+      //creamos una constante que tiene la URL de nuestra API
       const urlAPI = 'https://practprof2023-2855.restdb.io/rest/peliculas?apikey=6466d9870b60fc42f4e197bf';
-      const container = document.querySelector('#contenedorPeliculas');
-      let contentHTML = '';
+      //Creamos una constante que tendrá una referencia directa con el DIV "contenedorPeliculas" y nos permitirá cargarle contenido
+      const divContenedorPeliculas = document.querySelector('#contenedorPeliculas');
+      //cremos una variable vacía que contendrá todo el código HTML que vamos a insertar
+      let contenidoHTML = '';
+      //obtenemos las películas en formato Json
       fetch(urlAPI
       ).then(res => res.json())
         .then(datos => {
-          //console.log(datos[0]);
+          //enviamos a la consola de javascript lo obtenido
+          console.log(datos);
+          //recorremos la colección de películas obtenidas, obteniendo una referencia para cada una con la constante "peli", por cada iteración
           for (const peli of datos) {
-            let trailerUrl = peli.trailer_url;
-            contentHTML += `
+            contenidoHTML += `
               <div class="col-md-4">
-                  <a href="${trailerUrl}" target="_blank">
+                  <a href="${peli.trailer_url}" target="_blank">
                     <img src="${peli.portada_url}" alt="${peli.nombre}" class="img-thumbnail">
                   </a>
                   <h3 class="title">${peli.nombre}</h3>
+                  <a href="#" onclick="movies.eliminarPelicula('${peli._id}');" >Eliminar</a>
               </div>`;
           }
-          container.innerHTML = contentHTML;
+          divContenedorPeliculas.innerHTML = contenidoHTML;
         })
     },
     agregarNuevaPelicula:() => {
@@ -31,8 +38,20 @@ const movies = {
       body: JSON.stringify(nuevaPeli)
       })  
       .then(response => {
-        return movies.render();
+        console.log(response);
+        return movies.obtenerTodos();
       });
+    },
+    eliminarPelicula:(idPeliculaBorrar)=>{
+      alert("Borrando la película:");
+      const urlAPI = `https://practprof2023-2855.restdb.io/rest/peliculas/${idPeliculaBorrar}?apikey=6466d9870b60fc42f4e197bf`
+      fetch(urlAPI, {
+        method: 'DELETE'
+        })
+        .then(response => {
+          console.log(response);
+          return movies.obtenerTodos();
+        }); 
     }
   };
-  movies.render();
+  movies.obtenerTodos();
